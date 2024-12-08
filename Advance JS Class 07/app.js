e089f168fe68bc91cf2e177847b44dcf23d90a08
Adil Karim <muhammadadilmemon79@gmail.com>
2024-12-09 02:19:37 +0500

    Advance JS

---
+++ Advance JS Class 07/app.js
@@ -0,0 +1,116 @@
+// Function Constructor:
+
+
+// function Student(name,age,city) {
+//     this.stdName = name;
+//     this.stdAge = age;
+//     this.stdCity = city;
+//     this.getName = function () {
+//         return this.stdName;
+//     };
+// };
+
+// const std1 = new Student('adil',17,'karachi');
+// const std2 = new Student('hamza',20,'lahore');
+// const std3 = new Student('adnan',69,'pind');
+
+// console.log(std1.getName());
+// console.log(std2.getName());
+// console.log(std3.getName());
+
+// console.log(std1);
+// console.log(std2);
+// console.log(std3);
+
+
+
+// Class Constructor:
+
+
+// class Student{
+//     constructor (name,age,city) {
+//         this.stdName = name;
+//         this.stdAge = age;
+//         this.stdCity = city;
+//     };
+//     getName = () => {
+//         return this.stdName;
+//     };
+// };
+
+// const std01 = new Student('farooq',21,'karachi');
+// const std02 = new Student('sami',12,'america');
+
+// console.log(std01.getName());
+// console.log(std02.getName());
+
+// console.log(std01);
+// console.log(std02);
+
+
+
+// Inheriting:
+
+
+// class Student{
+//     constructor(name,age,city){
+//         this.stdName = name;
+//         this.stdAge = age;
+//         this.stdCity = city;
+//     };
+// };
+
+// class School extends Student{
+//     constructor (instituteName,instituteId,name,age,city) {
+//         super();
+//         this.stdName = name;
+//         this.stdAge = age;
+//         this.stdCity = city;
+//         this.schoolName = instituteName;
+//         this.schoolId = instituteId;
+//     };
+// };
+
+// const school01 = new School('JWP',135,'adil',17,'lahore');
+// const school02 = new School('DBS',264,'adnan shahid',69,'Lalu Khet 10 Number');
+
+// console.log(school01);
+// console.log(school02);
+
+
+
+// commonjs es5
+
+// es6 , modulejs
+
+
+
+// import , export:
+
+
+// const greet = () => {
+//     return "Muhammad Adil"
+// };
+
+
+// const stdObj = () => {
+//     return {
+//         name: 'Adil',
+//         age: 17,
+//         city: 'Karachi',
+//         email: 'muhammadadilmemon79@gmail.com',
+//     };
+// };
+
+
+// const stdSchool = () => {
+//     return {
+//         schoolId: 167,
+//         schoolName: 'JWP',
+//     };
+// };
+
+
+// export {greet, stdObj, stdSchool};
+
+
---
+++ Advance JS Class 07/app2.js
@@ -0,0 +1,13 @@
+// import , export:
+
+
+// import {greet, stdObj, stdSchool} from './app.js';
+
+
+// console.log(greet());
+
+// console.log(stdObj());
+
+// console.log(stdSchool());
+
+
